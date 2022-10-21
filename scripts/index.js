c2affbbdb964ad89342d43142b0f0dc53d7d001e
abdubosit-js <abdubositjs@gmail.com>
2022-10-22 01:03:43 +0500

    commit ikki

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,45 @@
-class Burger {
+let product = []
 
-    product = []
+function addCabbage(e) {
+    const cabbegediv = document.querySelector(".burger-material")
+    const cabbage = document.createElement('div')
+    
+    const id = e.dataset.id
+    let arr = product.push(id)
+    
+    localStorage.setItem("material", JSON.stringify(product))
+    const array1 = JSON.parse(localStorage.getItem('material'))
+    
+    array1.map(e => {
+        e == 0
+        if (Number(e) === 3) {
+            cabbage.classList.add("cabbage")
+            cabbegediv.append(cabbage)
+        }        
+    })
 
-    addCabbage(e) {
-        const id = e.dataset.id
-        localStorage.setItem("material", JSON.stringify(this.product))
-        const arr = this.product.push(JSON.parse(localStorage.getItem('material')))
-        
-        // const cabbegediv = document.querySelector(".burger-material")
-        // const cabbage = document.createElement('div')
-        // cabbage.classList.add("cabbage")
-        // cabbegediv.append(cabbage)
-    }
-
-    addCutlet(e) {
-        const id = e.dataset.id
-
-        const cabbegediv = document.querySelector(".burger-material")
-        const cabbage = document.createElement('div')
-        cabbage.classList.add("cutlet")
-        cabbegediv.append(cabbage)
-    }
-
-    addChease(e) {
-        const id = e.dataset.id
-
-        const cabbegediv = document.querySelector(".burger-material")        
-        const cabbage = document.createElement('div')
-        cabbage.classList.add("chease")
-        cabbegediv.append(cabbage)
-    }
-
-    card() {
-
-    }
 }
 
-const burger = new Burger()
+function addCutlet(e) {
+    const id = e.dataset.id
+
+    const cabbegediv = document.querySelector(".burger-material")
+    cabbegediv.insertAdjacentHTML("beforeend", `<div class="cutlet"></div>`)
+}
+
+function addChease(e) {
+    const id = e.dataset.id
+
+    const cabbegediv = document.querySelector(".burger-material")        
+    cabbegediv.insertAdjacentHTML("afterbegin", `<div class="chease"></div>`)
+}
+
+function card() {
+    const openCard = document.querySelector(".open-card")
+    openCard.style.left = "0px"
+}
+
+document.querySelector("#close").addEventListener("click", () => {
+    const openCard = document.querySelector(".open-card")
+    openCard.style.left = "-300px"
+})
