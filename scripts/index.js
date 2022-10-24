ed2f4ea53615c3b7a0442990c536130141e97e8f
abdubosit-js <abdubositjs@gmail.com>
2022-10-25 00:21:01 +0500

    o'zgardi

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,34 @@
-let product = []
+const products = JSON.parse(localStorage.getItem('material')) || []
 
-function addCabbage(e) {
-    const cabbegediv = document.querySelector(".burger-material")
-    const cabbage = document.createElement('div')
-    
-    const id = e.dataset.id
-    let arr = product.push(id)
-    
-    localStorage.setItem("material", JSON.stringify(product))
-    const array1 = JSON.parse(localStorage.getItem('material'))
-    
-    array1.map(e => {
-        e == 0
-        if (Number(e) === 3) {
-            cabbage.classList.add("cabbage")
-            cabbegediv.append(cabbage)
-        }        
-    })
+function renderProduct() {
+    const burgerDiv = document.querySelector(".burger-material");
+    burgerDiv.innerHTML = "";
 
+    for(let product of products) {        
+        const div = document.createElement('div')
+
+        if(Number(product) === 1){
+            div.classList.add("cutlet")
+        }
+        
+        if(Number(product) === 2){
+            div.classList.add("chease")
+        }
+        
+        if(Number(product) === 3){
+            div.classList.add("cabbage")
+        }
+
+        burgerDiv.appendChild(div)
+    }
 }
 
-function addCutlet(e) {
-    const id = e.dataset.id
-
-    const cabbegediv = document.querySelector(".burger-material")
-    cabbegediv.insertAdjacentHTML("beforeend", `<div class="cutlet"></div>`)
-}
-
-function addChease(e) {
-    const id = e.dataset.id
-
-    const cabbegediv = document.querySelector(".burger-material")        
-    cabbegediv.insertAdjacentHTML("afterbegin", `<div class="chease"></div>`)
+function addProduct({dataset}) {
+    products.push(dataset.id)
+    
+    localStorage.setItem("material", JSON.stringify(products))
+    renderProduct();
+    cartLength();
 }
 
 function card() {
@@ -43,3 +40,26 @@ document.querySelector("#close").addEventListener("click", () => {
     const openCard = document.querySelector(".open-card")
     openCard.style.left = "-300px"
 })
+
+function removeProduct({dataset}) {
+    const index = products.findIndex(product => product === dataset.id)
+    products.splice(index, 1)
+    localStorage.setItem("material", JSON.stringify(products))
+    renderProduct()
+    cartLength()
+}
+
+function cartLength() {
+    const cardDiv = document.querySelector(".price-cnt")
+    const length = document.querySelector(".length")
+    length.innerText = products.length
+    cardDiv.innerHTML = ""
+
+    const div = document.createElement("div")
+    div.classList.add("cutlet-price")
+    div.innerHTML =  "Products" + " - " + products.length
+    cardDiv.appendChild(div)
+}
+
+cartLength()
+renderProduct()
